e7dcb944f0ff3dcc085a3fd525b5967a10bce0a4
Dandee Galang <dandeegalang.ph@gmail.com>
2025-06-15 03:17:13 +0800

    added components directory

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
+import CardList from './components/card-list/card-list.component.jsx';
+
 
 class App extends Component {
   constructor() {
@@ -22,10 +23,18 @@ class App extends Component {
       .catch(error => console.error('Error fetching data:', error));  
   }
 
+  onSearch = (event) => {
+    const searchField = event.target.value.toLowerCase();
+    this.setState({ searchField: searchField });
+  }
+
   render() {
 
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    const { monsters, searchField } = this.state;
+    const { onSearch } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField.toLowerCase());
     });
 
     return (
@@ -34,20 +43,9 @@ class App extends Component {
           className='search-box' 
           type='search' 
           placeholder='search monsters' 
-          onChange={(event) => {
-            const searchField = event.target.value.toLowerCase();
-            this.setState({ searchField: searchField });
-          }}
+          onChange={onSearch}
         />
-        {
-          filteredMonsters.map((monster) => {
-            return (
-              <div key={monster.id} className="monster-card">
-                <h2>{monster.name}</h2>
-              </div> 
-            )
-          })
-        }
+        <CardList monsters={filteredMonsters}/>
       </div>
     );
   }
